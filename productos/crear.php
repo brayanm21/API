434fad9ed8f0718374d0fe6f467ac7b7c2e08707
brayanm21 <bmunozmasia@uniminuto.edu.co>
2021-04-11 15:16:59 -0500

    arreglo de post

--- productos/crear.php
+++ productos/crear.php
@@ -1,21 +1,25 @@
 <?php
+include_once "db.php";
+
 header("Access-Control-Allow-Origin: http://localhost:4200");
 header("Access-Control-Allow-Headers: *");
 $jsonProducto = file_get_contents("php://input");
 $objetoProductoPHP  = json_decode($jsonProducto,true);
-var_dump ($objetoProductoPHP);
-echo gettype($objetoProductoPHP["Descuento"]);
-if (!$objetoProductoPHP) {
-    exit("No hay datos");
-}
 
-    $db = include_once "db.php";
-    //$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
-$sentencia = $db->prepare("insert into productos(titulo, descripcion, precio, descuento, diasdescuento) values (?,?,?,?,?)");
-$resultado = $sentencia->execute([$objetoProductoPHP["Titulo"], $objetoProductoPHP["Descripcion"], $objetoProductoPHP["Precio"],$objetoProductoPHP["Descuento"],$objetoProductoPHP["Diasdescuento"]]);
-//print_r($db->errorInfo());
-echo json_encode([
+$Titulo = $objetoProductoPHP["Titulo"];
+$Descripcion = $objetoProductoPHP["Descripcion"];
+$Precio = $objetoProductoPHP["Precio"];
+$Descuento = $objetoProductoPHP["Descuento"];
+$Diasdescuento = $objetoProductoPHP["Diasdescuento"];
 
-    "resultado" => $resultado,
-]);
+$consulta = "INSERT INTO productos
+(titulo, descripcion, precio, descuento, diasdescuento) VALUES ('$Titulo','$Descripcion','$Precio','$Descuento','$Diasdescuento')";
+
+$connection = mysqli_connect("localhost","root","","cocoproyecto");
+if (mysqli_query($connection ,$consulta) ){
+    echo "<p>Registro agregado.</p>";
+    } else {
+    echo "<p>No se agregó...</p>";
+    }
+
 ?>
