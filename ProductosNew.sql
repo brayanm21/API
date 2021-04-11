7ddaad77f2c625f2b39b1b5745a4abd238b6af70
brayanm21 <bmunozmasia@uniminuto.edu.co>
2021-04-11 11:55:24 -0500

    script

--- ProductosNew.sql
+++ ProductosNew.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-04-2021 a las 04:02:11
+-- Tiempo de generación: 11-04-2021 a las 18:53:30
 -- Versión del servidor: 10.4.18-MariaDB
 -- Versión de PHP: 7.3.27
 
